01cd9ab0970fff9502873eb1f3c07af9f417f696
Emmanuel Fofeyin Ngwamunwi <emmanuel.fofeyin-ngwamunwi@mths.ca>
2022-10-14 15:05:02 +0000

    made changes

---
+++ ICS3U-Unit3-02.cpp
@@ -0,0 +1,28 @@
+// Copyright (c) 2022 St. Mother Teresa HS All rights reserved.
+//
+// Created by: Emmanuel
+// Created on: oct 2022
+// This program is a number guessing game
+
+#include <iomanip>
+#include <iostream>
+
+int main() {
+    // creating variables
+    const int NUMBER = 5;
+    int guessNumber;
+
+    // input
+    std::cout << "Guess a number from 0 - 9: ";
+    std::cin >> guessNumber;
+
+    // process and output
+    if (NUMBER == guessNumber) {
+        std::cout << "\nYou guessed right.";
+    }
+    if (NUMBER != guessNumber) {
+        std::cout << "\nYou guessed wrong, try again.";
+    }
+
+    std::cout << "\n\n\nDone.\n";
+}
